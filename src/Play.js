eb55c6d72a9124555c84921da00e5a1041844857
ChristopherTGreen <ctgreen00@gmail.com>
2026-01-30 14:04:12 -0800

    first commit

---
+++ src/Play.js
@@ -0,0 +1,64 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super('playScene')
+    }
+
+    init() {
+        // useful variables
+        this.SHOT_VELOCITY_X = 200
+        this.SHOT_VELOCITY_Y_MIN = 700
+        this.SHOW_VELOCITY_Y_MAX = 1100
+
+    }
+
+    preload() {
+        this.load.path = './assets/img/'
+        this.load.image('grass', 'grass.jpg')
+        this.load.image('cup', 'cup.jpg')
+        this.load.image('ball', 'ball.png')
+        this.load.image('wall', 'wall.png')
+        this.load.image('oneway', 'one_way_wall.png')
+    }
+
+    create() {
+        // add background grass
+        this.grass = this.add.image(0, 0, 'grass').setOrigin(0)
+
+        // add cup
+        this.cup = this.physics.add.sprite(width / 2, height / 10, 'cup')
+        this.cup.body.setCircle(this.cup.width / 4)
+        this.cup.body.setOffset(this.cup.width / 4)
+        this.cup.body.setImmovable(true)
+
+        // add ball
+        this.ball = this.physics.add.sprite(width / 2, height - height / 10, 'ball')
+        this.ball.body.setCircle(this.ball.width / 2)
+        this.ball.setCollideWorldBounds(true)
+        this.ball.body.setBounce(0.5)
+        this.ball.body.setDamping(true).setDrag(0.5)
+
+        // add walls
+
+        // add one-way
+
+        // add pointer input
+
+        // cup/ball collision
+
+        // ball/wall collision
+
+        // ball/one-way collision
+    }
+
+    update() {
+
+    }
+}
+/*
+CODE CHALLENGE
+Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
+[ ] Add ball reset logic on successful shot
+[ ] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
+[ ] Make one obstacle move left/right and bounce against screen edges
+[ ] Create and display shot counter, score, and successful shot percentage
+*/
