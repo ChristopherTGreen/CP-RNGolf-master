5b0d3848281a549e75b9c5bd392c8df45ec306b2
ChristopherTGreen <ctgreen00@gmail.com>
2026-01-30 15:44:58 -0800

    4/4 tasks

--- src/Play.js
+++ src/Play.js
@@ -5,10 +5,14 @@ class Play extends Phaser.Scene {
 
     init() {
         // useful variables
-        this.SHOT_VELOCITY_X = 200
+        this.SHOT_VELOCITY_X = 300
         this.SHOT_VELOCITY_Y_MIN = 700
         this.SHOW_VELOCITY_Y_MAX = 1100
+        this.WALL_SPEED = 100
 
+        // other variables 
+        this.shots = 0
+        this.holes = 0
     }
 
     preload() {
@@ -21,8 +25,20 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        // initial score config
+        let scoreConfig = {
+            fontSize: '50px',
+            fontFamily: 'Arial',
+            color: '#82c4f3',
+        }
+
         // add background grass
         this.grass = this.add.image(0, 0, 'grass').setOrigin(0)
+
+        // display
+        this.displayShots = this.add.text(0, 0, "Shots: " + this.shots, scoreConfig)
+        this.displayHoles = this.add.text(0, 40, "Holes: "+ this.holes, scoreConfig)
+        this.displayRate = this.add.text(0, 80, "Rate: 0", scoreConfig)
 
         // add cup
         this.cup = this.physics.add.sprite(width / 2, height / 10, 'cup')
@@ -33,21 +49,61 @@ class Play extends Phaser.Scene {
         // add ball
         this.ball = this.physics.add.sprite(width / 2, height - height / 10, 'ball')
         this.ball.body.setCircle(this.ball.width / 2)
-        this.ball.setCollideWorldBounds(true)
+        this.ball.body.setCollideWorldBounds(true)
         this.ball.body.setBounce(0.5)
         this.ball.body.setDamping(true).setDrag(0.5)
 
         // add walls
+        this.wallA = this.physics.add.sprite(0, height/4, 'wall')
+        this.wallA.setX(Phaser.Math.Between(0 + this.wallA.width / 2, width - this.wallA.width / 2))
+        this.wallA.body.setCollideWorldBounds(true)
+        this.wallA.body.setImmovable(true)
+        this.wallA.body.setVelocityX(this.WALL_SPEED)
+        this.wallA.body.setBounce(1.0)
+
+        let wallB = this.physics.add.sprite(0, height/2, 'wall')
+        wallB.setX(Phaser.Math.Between(0 + wallB.width / 2, width - wallB.width / 2))
+        wallB.body.setImmovable(true)
+
+        this.walls = this.add.group([this.wallA, wallB])
 
         // add one-way
+        this.oneWay = this.physics.add.sprite(width / 2, height / 4 * 3, 'oneway')
+        this.oneWay.setX(Phaser.Math.Between(0 + this.oneWay.width / 2, width - this.oneWay.width / 2))
+        this.oneWay.body.setImmovable(true)
+        this.oneWay.body.checkCollision.down = false
 
         // add pointer input
+        this.input.on('pointerdown', (pointer) => {
+            let shotDirection = pointer.y <= this.ball.y ? 1 : -1
+            let shotRelative = pointer.x <= this.ball.x ? 1 : -1
+            this.ball.body.setVelocityX(shotRelative * Phaser.Math.Between(100, this.SHOT_VELOCITY_X))
+            this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOW_VELOCITY_Y_MAX) * shotDirection)
+            
+            // shots counter
+            this.shots+=1
+            this.displayShots.setText("Shots: " + this.shots)
+            this.displayRate.setText("Rate: " + Phaser.Math.RoundTo((this.holes/this.shots), -2, 10))
+        })
 
         // cup/ball collision
+        this.physics.add.collider(this.ball, this.cup, (ball, cup) => {
+            this.ball.setPosition(width / 2, height - height / 10)
+            this.ball.setX(Phaser.Math.Between(0 + ball.width / 2, width - ball.width / 2))
+            this.ball.setVelocity(0, 0)
+
+            // holes counter
+            this.holes += 1
+            this.displayHoles.setText("Holes: " + this.holes)
+            this.displayRate.setText("Rate: " + Phaser.Math.RoundTo((this.holes/this.shots), -2, 10))
+        })
 
         // ball/wall collision
+        this.physics.add.collider(this.ball, this.walls)
 
         // ball/one-way collision
+        this.physics.add.collider(this.ball, this.oneWay)
+
     }
 
     update() {
@@ -57,8 +113,8 @@ class Play extends Phaser.Scene {
 /*
 CODE CHALLENGE
 Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
-[ ] Add ball reset logic on successful shot
-[ ] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
-[ ] Make one obstacle move left/right and bounce against screen edges
-[ ] Create and display shot counter, score, and successful shot percentage
+[ x ] Add ball reset logic on successful shot
+[ x ] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
+[ x ] Make one obstacle move left/right and bounce against screen edges
+[ x ] Create and display shot counter, score, and successful shot percentage
 */
